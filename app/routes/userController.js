8e8d788416cb04c97bc3b30187632e2e10c99f36
Rostradamus <hyungro@hotmail.com>
2019-03-01 06:01:10 +0000

    Administration module API setup
    What:
    - Fix Query Manager to prevent SQL injection
    - Implement Administration API based on API Spec
    - Fix indentation
    
    Why:
    - N/A
    
    Branch: TD-11
    Topic: administration API
    Action: add

--- app/routes/userController.js
+++ app/routes/userController.js
@@ -1,41 +1,68 @@
 const routes = require('express').Router();
-const qm = require("@app/helpers/queryManager");
+const userManager = require("@app/helpers/queryManager/user");
 
 routes.get("/", (req, res) => {
-    qm.getAllActiveUsers().then((result) => {
+  userManager.getAllActiveUsers()
+    .then(result => {
       res.status(200);
       res.send(result);
-    }).catch((err) => {
-        res.status(500).json(err);
+    })
+    .catch(err => {
+      res.status(500).json(err);
     });
 });
 
-routes.get("/:username", (req, res) => {
-    qm.getUserWithUsernameFromTable("User", req.params.username).then((result) => {
-        res.status(200);
-        res.send(result);
-    }).catch((err) => {
-        res.status(500).json(err);
+routes.get("/:user_id", (req, res) => {
+  userManager.getUserWithIdFromTable(req.params.user_id)
+    .then(result => {
+      if (result.length === 0) {
+        res.status(404)
+          .json({ message: `User with id = ${req.params.user_id} does NOT exit`});
+      }
+      res.status(200);
+      res.send(result[0]);
+    }).catch(err => {
+      res.status(500).json(err);
     });
 });
 
 routes.post("/", (req, res) => {
-    res.status(200);
-    res.send({data: "Hello World"});
-});
+  userManager.createUser(req.body)
+    .then(result => {
+      if (result.length === 0)
+        throw new Error();
 
-routes.put("/", (req, res) => {
-    res.status(200);
-    res.send({data: "Hello World"});
-});
-
-routes.delete("/:username", (req, res) => {
-    qm.deleteUserWithUsername(req.params.username).then((result, err) => {
-        res.status(200);
-        res.send(result);
-    }).catch((err) => {
-        res.status(500).json(err);
+      res.status(200);
+      res.send(result[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
     });
-})
+});
+
+routes.put("/:user_id", (req, res) => {
+  userManager.updateUserWithId(req.params.user_id, req.body)
+    .then(result => {
+      if (result.length === 0)
+        res.sendStatus(404);
+      res.status(200);
+      res.send(result[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+routes.delete("/:user_id", (req, res) => {
+  userManager.deleteUserWithId(req.params.user_id)
+    .then(result => {
+      if (result.affectedRows === 0)
+        res.sendStatus(404);
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = routes;
--- config/db/connection.js
+++ config/db/connection.js
@@ -2,24 +2,24 @@ const mysql = require('mysql');
 let db;
 
 module.exports = {
-    connectDatabase: function() {
-        if (!db) {
-            db = mysql.createConnection({
-            host     : process.env.DB_HOST,
-            user     : process.env.DB_USERNAME,
-            password : process.env.DB_PASSWORD,
-            database : process.env.DB_NAME
-            });
+  connectDatabase: function() {
+    if (!db) {
+      db = mysql.createConnection({
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USERNAME,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME
+      });
 
-            db.connect(err => {
-                if(!err) {
-                    console.log('Database is connected!');
-                } else {
-                    console.log(err);
-                    console.log('Error connecting database!');
-                }
-            });
+      db.connect(err => {
+        if(!err) {
+          console.log('Database is connected!');
+        } else {
+          console.log(err);
+          console.log('Error connecting database!');
         }
-        return db;
+      });
     }
+    return db;
+  }
 }
