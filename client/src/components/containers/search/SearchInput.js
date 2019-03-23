f98bb4967588ca93b0d4067f8f407b9dd8d67bda
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2019-03-23 20:51:04 +0000

    block users from changed selectedUsers in searchFields, block users from changing time to before or after work hours

--- client/src/components/containers/search/SearchInput.js
+++ client/src/components/containers/search/SearchInput.js
@@ -4,69 +4,68 @@ import { Search } from "semantic-ui-react";
 class SearchInput extends Component {
   constructor(props) {
     super(props);
+    const {formType, results, selectedUser} = props;
     this.state = {
-      type: "",
-      selectedUser: {},
-      results: [],
-      filteredResults: [],
-      searchText: ""
+      formType: formType,
+      selectedUser: selectedUser,
+      filteredResults: this._initialFilterResult(results, formType),
+      searchText: selectedUser ? `${selectedUser.first_name} ${selectedUser.last_name}` : ""
     }
 
     this.handleResultSelect = this.handleResultSelect.bind(this);
     this.handleSearchChange = this.handleSearchChange.bind(this);
   }
 
-  static getDerivedStateFromProps(nextProps, prevState){
-    const {type, results, selectedUser} = nextProps;
-    if (type !== prevState.type && results) {
-      const activeResults = results.filter(user => user.type === nextProps.type)
-        .map(user => Object.assign({...user, title : `${user.first_name} ${user.last_name}`}));
-
-      if (selectedUser) {
-        return {
-          results: activeResults,
-          type: nextProps.type,
-          selectedUser: nextProps.selectedUser,
-          searchText: `${selectedUser.first_name} ${selectedUser.last_name}`
-        };
-      } else {
-        return {
-          results: activeResults,
-          type: nextProps.type,
-        };
-      }
-
-    }
-    return {};
+  /**
+   * It will return an arry of user with user.formType === param.formType
+   * @param  {Array}  results [patients and staffs passed down as props]
+   * @param  {String} formType    [Enum: "Patient", "Staff"]
+   * @return {Array}         [filtered results with elements that have formType === param.formType]
+   */
+  _initialFilterResult(results = [], formType) {
+    return results.filter(user => user.type === formType);
   }
 
   handleResultSelect(e, { result }) {
     if (result) {
-      const { type } = this.state;
-      this.setState({
-        selectedUser : result,
-        searchText: `${result.first_name} ${result.last_name}`
-      });
-      this.props.handleSearchInputSelect(result, type);
+      const { formType, selectedUser } = this.state;
+      // isSelectedUser is a boolean object that is true if selectedUser is the current user's view otherwise false
+      const { isSelectedUser } = this.props;
+
+      if (!isSelectedUser) {
+        this.setState({
+          selectedUser : result,
+          searchText: `${result.first_name} ${result.last_name}`
+        });
+        this.props.handleSearchInputSelect(result, formType);
+      }
     }
   }
 
   handleSearchChange(e, { value }) {
-    const filteredResults = (this.state.results || [])
-      .filter(user =>
-        (user.title && user.title.toLowerCase().includes(value.toLowerCase())) ||
-        user.email.toLowerCase().includes(value.toLowerCase()));
-    this.setState({ searchText: value, filteredResults});
+    const { formType, selectedUser } = this.state;
+    // isSelectedUser is a boolean object that is true if selectedUser is the current user's view otherwise false
+    const { isSelectedUser } = this.props;
+
+    if (!isSelectedUser) {
+      const filteredResults = (this.props.results || [])
+        .filter(user => ( user.title && user.title.toLowerCase().includes(value.toLowerCase())) &&
+         user.type === formType);
+      this.setState({ searchText: value, filteredResults});
+      this.props.handleSearchInputSelect({}, formType);
+    }
   }
 
   render() {
-    const { filteredResults, results, searchText } = this.state;
+    const { filteredResults, searchText, selectedUser } = this.state;
+    const { isSelectedUser } = this.props;
     return(
       <div>
         <Search
+          disabled={ isSelectedUser }
           onResultSelect={ this.handleResultSelect }
           onSearchChange={ this.handleSearchChange }
-          results={ filteredResults || results || [] }
+          results={ !isSelectedUser ? filteredResults : [selectedUser] }
           value={ searchText }
         />
       </div>
--- client/src/reducers/PatientStaffSearchReducer.js
+++ client/src/reducers/PatientStaffSearchReducer.js
@@ -5,7 +5,9 @@ const initialState = [];
 export default (state = initialState, action) => {
   switch (action.type) {
     case PATIENT_STAFF_SEARCH_ACTION_TYPE.FETCH_SUCCESS : {
-      const filteredResult = action.payload.items.filter(user => user.type === "Patient" || user.type === "Staff");
+      const filteredResult = action.payload.items
+        .filter(user => user.type === "Patient" || user.type === "Staff")
+        .map(user => Object.assign({...user, title : `${user.first_name} ${user.last_name}`}));
       return filteredResult;
     }
     case PATIENT_STAFF_SEARCH_ACTION_TYPE.FETCH_FAILURE: {
