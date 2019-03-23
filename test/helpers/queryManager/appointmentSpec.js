d9d56cccb9214832e73c92ebc1a663550e72ac9b
Mike Yoon <mikeyoon@hotmail.ca>
2019-03-23 18:49:08 +0000

    Merge branch 'master' of gitlab.com:cpsc319-2018w2/coastal-health/dokdo/schedule-system into 40-email-integrate-appointment

--- test/helpers/queryManager/appointmentSpec.js
+++ test/helpers/queryManager/appointmentSpec.js
@@ -1,27 +1,27 @@
-describe('appointmentQueryManager unit test', () => {
-  let appointmentQueryManager;
+// describe('appointmentQueryManager unit test', () => {
+//   let appointmentQueryManager;
 
-  before(() => {
-    appointmentQueryManager = require('@app/helpers/queryManager/appointment');
-  });
+//   before(() => {
+//     appointmentQueryManager = require('@app/helpers/queryManager/appointment');
+//   });
 
-  describe("getAppointmentAccordingToUser", ()=> {
-    it("user with id= 6 type= 'Staff' should return result length of 2", async () => {
-      const id = 6, type = "Staff";
-      let getAppointmentAccordingToUserSpy = spy(appointmentQueryManager, "getAppointmentAccordingToUser");
-      expect(getAppointmentAccordingToUserSpy.callCount).to.equal(0);
+//   describe("getAppointmentAccordingToUser", ()=> {
+//     it("user with id= 6 type= 'Staff' should return result length of 2", async () => {
+//       const id = 6, type = "Staff";
+//       let getAppointmentAccordingToUserSpy = spy(appointmentQueryManager, "getAppointmentAccordingToUser");
+//       expect(getAppointmentAccordingToUserSpy.callCount).to.equal(0);
 
-      const res = await getAppointmentAccordingToUserSpy(id, type);
-      expect(res.length).to.equal(2);
-      expect(getAppointmentAccordingToUserSpy.callCount).to.equal(1);
+//       const res = await getAppointmentAccordingToUserSpy(id, type);
+//       expect(res.length).to.equal(2);
+//       expect(getAppointmentAccordingToUserSpy.callCount).to.equal(1);
 
-      getAppointmentAccordingToUserSpy.restore();
-    });
+//       getAppointmentAccordingToUserSpy.restore();
+//     });
 
-    it("user with id= -1 type= '' should return result length of 0", async () => {
-      const id = -1, type = "";
-      const res = await appointmentQueryManager.getAppointmentAccordingToUser(id, type);
-      expect(res.length).to.equal(0);
-    });
-  });
-});
+//     it("user with id= -1 type= '' should return result length of 0", async () => {
+//       const id = -1, type = "";
+//       const res = await appointmentQueryManager.getAppointmentAccordingToUser(id, type);
+//       expect(res.length).to.equal(0);
+//     });
+//   });
+// });
