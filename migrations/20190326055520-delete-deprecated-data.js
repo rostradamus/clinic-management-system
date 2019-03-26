35e2ab4fd143a3cd55841c36fe0da9efca35dbb1
Rostradamus <hyungro@hotmail.com>
2019-03-26 06:11:38 +0000

    Resolve "Add migration for delete deprecated appointments"

---
+++ migrations/20190326055520-delete-deprecated-data.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var dbm = dbm || require('db-migrate');
+var type = dbm.dataType;
+var fs = require('fs');
+var path = require('path');
+
+exports.up = function(db, callback) {
+  let filePath = path.join(__dirname + '/sqls/20190326055520-delete-deprecated-data-up.sql');
+  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (err) return console.log(err);
+    db.runSql(data, function(err) {
+      if (err) return console.log(err);
+      callback();
+    });
+  });
+};
+
+exports.down = function(db) {
+  return null;
+};
