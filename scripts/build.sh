920a369ece43334699308aec7d09a7babf5c62d2
Ro Lee <rolee0429@gmail.com>
2019-04-01 21:36:07 -0700

    trouble debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,12 +12,11 @@ git reset --hard origin/master
 echo "=============RUNNING BACKEND LIBRARY IMPORT JOB============="
 rm -rf node_modules
 NODE_ENV=production npm install
-cd ./client
 echo "=============RUNNING FRONTEND LIBRARY IMPORT JOB============="
+cd ./client
+pwd
 rm -rf node_modules
 npm install
 NODE_ENV=production npm run build
-echo "=============RUNNING BUILD JOB============="
-cd ../
 echo "=============DATABASE MIGRATION============="
 NODE_ENV=production node node_modules/db-migrate/bin/db-migrate up --verbose
