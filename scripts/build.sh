c4270d8f28a2bb49c85795ceb4f7bd4ccf46056c
Ro Lee <rolee0429@gmail.com>
2019-04-01 21:09:49 -0700

    quick fix

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+npm bin
+echo "=============NPM VERSION============="
+npm -v
+echo "=============NPM VERSION============="
+node -v
+echo "=============RUNNING GIT CHECKOUT JOB============="
+git checkout master
+echo "=============PULLING MOST RECENT HEAD============="
+git fetch
+git reset --hard origin/master
+echo "=============RUNNING BACKEND LIBRARY IMPORT JOB============="
+rm -rf node_modules
+NODE_ENV=production npm install
+cd ./client
+echo "=============RUNNING FRONTEND LIBRARY IMPORT JOB============="
+rm -rf node_modules
+npm install && NODE_ENV=production npm run build
+echo "=============RUNNING BUILD JOB============="
+cd ../
+echo "=============DATABASE MIGRATION============="
+NODE_ENV=production node node_modules/db-migrate/bin/db-migrate up --verbose
